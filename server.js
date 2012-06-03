54b411302fa871b27b354fcb4dcd1204cd8b9a56
Christian Vuerings <vueringschristian@gmail.com>
2012-06-03 11:33:10 -0700

    Update index.html

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.register('.html', require('jade'));
+app.set('views', __dirname + '/views');
+app.set('view options', { layout: false });
 
 app.get('/', function(request, response) {
-  response.send('Hello Christian!');
+    response.render('index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
